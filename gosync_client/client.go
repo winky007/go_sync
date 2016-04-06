55bac015dda7a3d0ff0b51c4f9bde12430ad0af6
Bill <winky007@163.com>
2016-04-06 17:36:36 +0800

    add sync folder func

--- gosync_client/client.go
+++ gosync_client/client.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -16,7 +18,7 @@ func usage() {
 	fmt.Println("Usage: \n")
 	fmt.Println("-host etc: 127.0.0.1\n")
 	fmt.Println("-port etc: 8989\n")
-	fmt.Println("-file file path, etc: /var/log.txt\n")
+	fmt.Println("-local local folder path, etc: /var/log\n")
 	fmt.Println("-dest remote server destination folder, etc: /tmp\n")
 }
 
@@ -25,22 +27,22 @@ func main() {
 		usage()
 		return
 	}
-	filePtr := flag.String("file", "", "file path; etc: /var/log.txt")
+	localPtr := flag.String("local", "", "local folder path; etc: /var/log.txt")
 	hostPtr := flag.String("host", "", "host; etc: 127.0.0.1")
 	portPtr := flag.String("port", "", "port; etc: 8989")
 	destPtr := flag.String("dest", "", "etc: /tmp")
 	flag.Parse()
-	file := *filePtr
+	local := *localPtr
 	host := *hostPtr
 	port := *portPtr
 	dest := *destPtr
 
-	if file == "" {
-		log.Fatal("invalid file path")
+	if local == "" {
+		log.Fatal("invalid local folder path")
 		os.Exit(1)
 	}
-	if !fileExist(file) {
-		log.Fatal("file no exist...")
+	if !fileExist(local) {
+		log.Fatal("local folder no exist...")
 		os.Exit(1)
 	}
 
@@ -52,24 +54,46 @@ func main() {
 	echoProtocol := &echo.EchoProtocol{}
 
 	for {
-		str, err := getFileContent(file)
+		fileList := []string{}
+		err := filepath.Walk(local, func(path string, f os.FileInfo, err error) error {
+			if !f.IsDir() {
+				fileList = append(fileList, path)
+			}
+			return nil
+		})
+
 		if err != nil {
 			log.Fatal(err)
 		}
+		local = strings.TrimRight(local, `/`)
+		dir := strings.Split(local, `/`)
+		parentDirS := dir[0 : len(dir)-1]
+		parentDir := strings.Join(parentDirS, `/`) + `/`
+		for _, file := range fileList {
+			str, err := getFileContent(file)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		fmd5, err := md5String(str)
-		if err != nil {
-			log.Fatal(err)
+			fmd5, err := md5String(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			destNew := strings.TrimRight(dest, "/")
+			destNew = destNew + "/"
+			destNew = destNew + strings.Replace(file, parentDir, "", 1)
+			destNewS := strings.Split(destNew, "/")
+			destNewS = destNewS[0 : len(destNewS)-1]
+			destNew = strings.Join(destNewS, "/") + "/"
+			data := file + "##%%^^##" + str + "##%%^^##" + fmd5 + "##%%^^##" + destNew
+			conn.Write(echo.NewEchoPacket([]byte(data), false).Serialize())
+			p, err := echoProtocol.ReadPacket(conn)
+			if err == nil {
+				echoPacket := p.(*echo.EchoPacket)
+				fmt.Printf("Server reply:[%v] [%v]\n", echoPacket.GetLength(), string(echoPacket.GetBody()))
+			}
 		}
-		data := file + "##%%^^##" + str + "##%%^^##" + fmd5 + "##%%^^##" + dest
-		conn.Write(echo.NewEchoPacket([]byte(data), false).Serialize())
-		p, err := echoProtocol.ReadPacket(conn)
-		if err == nil {
-			echoPacket := p.(*echo.EchoPacket)
-			fmt.Printf("Server reply:[%v] [%v]\n", echoPacket.GetLength(), string(echoPacket.GetBody()))
-		}
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
 
 	conn.Close()
